ea44ac1043561471ebdd44d7eaf5375f6f5500c4
youghimire <youghimire@gmail.com>
2019-12-14 23:02:40 +0545

    Product display

--- src/javaapplication1/JavaApplication1.java
+++ src/javaapplication1/JavaApplication1.java
@@ -20,13 +20,13 @@ public class JavaApplication1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) throws Exception {
-        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/hms", "root", "root");
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/onlinestore", "root", "root");
         Statement statement=connection.createStatement();  
-        ResultSet resultSet=statement.executeQuery("select * from doctor");
-        System.out.println("name\t\tphone");
+        ResultSet resultSet=statement.executeQuery("select * from products");
+        System.out.println("Name\t\tPrice\t\tStock");
         while(resultSet.next())  
-            System.out.println(resultSet.getString(2)+"\t"+resultSet.getString(3));  
-        connection.close();  
+            System.out.println(resultSet.getString(3)+"\t\tRs"+resultSet.getString(4)+"\t\t"+resultSet.getString(7)+resultSet.getString(6));  
+        connection.close();
     }
     
 }
