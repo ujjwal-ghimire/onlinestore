c96442a67c8406c5d1fcefff4b640a0099ac6973
youghimire <youghimire@gmail.com>
2019-12-15 22:37:48 +0545

    Added Repository and Connection classes

---
+++ src/nccs/onlinestore/Product.java
@@ -0,0 +1,31 @@
+
+package nccs.onlinestore;
+
+public class Product {
+    
+    int product_id;
+    int category_id;
+    String name;
+    float price;
+    int discount;
+    String unit;
+    int stock;
+
+    public Product(int product_id, int category_id, String name, float price, int discount, String unit, int stock) {
+        this.product_id = product_id;
+        this.category_id = category_id;
+        this.name = name;
+        this.price = price;
+        this.discount = discount;
+        this.unit = unit;
+        this.stock = stock;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" + "product_id=" + product_id + ", category_id=" + category_id + ", name=" + name + ", price=" + price + ", discount=" + discount + ", unit=" + unit + ", stock=" + stock + '}';
+    }
+    
+    
+    
+}
